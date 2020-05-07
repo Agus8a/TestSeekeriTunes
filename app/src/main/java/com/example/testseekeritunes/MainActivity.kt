27574c18e3306f8fdb72320f79a60c4184026521
David Figueroa <dgfigueroa29@gmail.com>
2020-05-07 19:23:37 -0300

    Start project with Fragment + ViewModel template

---
+++ app/src/main/java/com/example/testseekeritunes/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.testseekeritunes
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.testseekeritunes.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
